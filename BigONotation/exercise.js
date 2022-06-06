f6e146c6d8ab8f1ca8cc924240600338545862b0
uneyss <73793862+uneyss@users.noreply.github.com>
2022-06-06 19:32:28 +0300

    Big O Notation Seciton
    Big O Notation Section
    
    -Added-
    *Some simple code examples about Big O Notation.

--- BigONotation/exercise.js
+++ BigONotation/exercise.js
@@ -1,10 +1,11 @@
-// Big O N -> Kaç verirsek verelim yazdırıcak. 
+// O(n) -> Kaç verirsek verelim yazdırıcak. 
 function bigon(n){
     for(let i=0; i < n; i++){
         console.log("Big O N değerleri yazdırılıyor: ", i)
     }
 }
 
+// O(n^2)
 function bigon2(n,j){
     for(let i=0; i < n; i++){
         console.log("Big O N değerleri yazdırılıyor: i = ", i)
@@ -15,5 +16,40 @@ function bigon2(n,j){
 
 }
 
+// O(log n)
+function logn(n){
+    while (n > 1) {
+        n = Math.floor(n/2)
+        console.log("Log n değerleri yazdırılıyor: n",n)
+    }
+}
+
+// O(n log(n))
+function nlogn(n){
+    lim = n
+    while (n > 1){
+        n = Math.floor(n/2)
+        for(let i = 0; i < lim; i++){
+            console.log(i)
+        }
+    }
+}
+
+// O(n!)
+function nfactorail(n){
+    if(n == 0){
+        return console.log("0 a eşit",n)
+    }
+    else{
+        for(let i = 0; i < n; i++){
+            console.log(i)
+            nfactorail(n-1)
+        }
+    }
+}
+
 bigon(5)
 bigon2(5)
+logn(100)
+nlogn(16)
+console.log(nfactorail(5))
